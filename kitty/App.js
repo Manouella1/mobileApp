af2bb61949116881e627225fa41f309b1cdc7daf
Manouella1 <138385119+Manouella1@users.noreply.github.com>
2024-08-21 11:31:13 +0200

    json

--- kitty/App.js
+++ kitty/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hejj!</Text>
+      <Text>{JSON.stringify(kitties)}</Text>
       <StatusBar style="auto" />
     </View>
   );
