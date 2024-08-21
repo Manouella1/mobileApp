73f7cf24bcc2e0e2035e9ad8f708f24f530d05ee
Manouella1 <138385119+Manouella1@users.noreply.github.com>
2024-08-21 11:34:20 +0200

    hej

--- kitty/App.js
+++ kitty/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [kitties, setKitties] = useState([]);
   return (
     <View style={styles.container}>
       <Text>{JSON.stringify(kitties)}</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> jhwsjk
     </View>
   );
 }
